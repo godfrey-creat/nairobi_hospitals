4db1852b3f29b987bac981f560025ac4d28f2bbe
godfrey-creat <ogodfrey135@gmail.com>
2024-02-29 12:19:31 +0300

    m

--- api/v1/app.py
+++ api/v1/app.py
@@ -82,8 +82,8 @@ def login_route():
                 return render_template('forms/login.html', form=form,msg='Invalid credentials')
             if user and bcrypt.check_password_hash(user.password, password):
                 # Authentication successful, redirect to dashboard or user-specific route
-                #return render_template('home.html',companies=companies,test='godfrey')
-                return redirect(url_for('home.html', hospitals=hospitals, test='peter'))
+                return render_template('home.html',companies=companies,test='godfrey')
+                #return redirect(url_for('home.html', hospitals=hospitals, test='peter'))
             else:
                 return render_template('forms/login.html', form=form,msg='Invalid credentials')
     return render_template('forms/login.html', form=form)
